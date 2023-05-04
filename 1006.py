076ffbacb73162ac50d4e65d513b0962dd5328cc
paulotozi <77864606+paulotozi@users.noreply.github.com>
2023-05-04 20:36:51 -0300

    Create 1006.py

---
+++ 1006.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+MEDIA = ((2 * A) + (3 * B) + (5 * C)) / 10
+
+print("MEDIA = {}".format("{:.1f}".format(MEDIA)))
