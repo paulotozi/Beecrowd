772215a202cb4f4b0db722ecd08541e6c0c39366
paulotozi <paulotozi1512@gmail.com>
2023-05-07 14:54:42 -0300

    Adicionando 1050.py

---
+++ Python/1050.py
@@ -0,0 +1,37 @@
+N = int(input())
+
+if(N == 61):
+
+    print("Brasilia")
+
+elif(N == 71):
+
+    print("Salvador")
+
+elif(N == 11):
+
+    print("Sao Paulo")
+
+elif(N == 21):
+
+    print("Rio de Janeiro")
+
+elif(N == 32):
+
+    print("Juiz de Fora")
+
+elif(N == 19):
+
+    print("Campinas")
+
+elif(N == 27):
+
+    print("Vitoria")
+
+elif(N == 31):
+
+    print("Belo Horizonte")
+
+elif(N != 61 and N != 71 and N != 11 and N != 21 and N != 32 and N != 19 and N != 27 and N != 31):
+
+    print("DDD nao cadastrado")
