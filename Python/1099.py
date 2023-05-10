e2cd42a6b673610f103c08c886c447aeac3f755a
paulotozi <paulotozi1512@gmail.com>
2023-05-10 00:34:59 -0300

    Adicionando 1099.py

--- Python/1099.py
+++ Python/1099.py
@@ -1,3 +1,16 @@
-if(2.0 % 1 == 0):
+N = int(input())
 
-    print("aqui")
+for _ in range(N):
+
+    sum = 0
+    num = input().split()
+    n1, n2 = num
+    arr = [int(n1)] + [int(n2)]
+    arr.sort()
+    for i in range(arr[0] + 1, arr[1]):
+
+        if(i % 2 != 0):
+
+            sum += i
+    
+    print(sum)
