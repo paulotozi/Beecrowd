936c4ddd66795bf834f36717e96ac9339f51f1af
paulotozi <paulotozi1512@gmail.com>
2023-05-12 19:47:18 -0300

    Adicionando 1150.py

---
+++ Python/1150.py
@@ -0,0 +1,23 @@
+x = int(input())
+z = int(input())
+temp = x
+i = 0
+soma = 0
+
+while(z <= x):
+
+    if(z <= x):
+
+        z = int(input())
+
+    if(z > x):
+
+        break
+
+while(soma <= z):
+
+    soma += temp
+    i += 1
+    temp += 1
+
+print(i)
