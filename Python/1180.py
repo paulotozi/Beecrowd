4fc5d53bf4667882faa6f039320e0025c918c585
paulotozi <paulotozi1512@gmail.com>
2023-05-14 19:39:20 -0300

    Adicionando 1180.py

---
+++ Python/1180.py
@@ -0,0 +1,21 @@
+N = int(input())
+arr = input().split()
+print(N)
+menor = 1000
+idx = 0
+
+for i in range(len(arr)):
+
+    num = int(arr[i])
+    if(num < menor):
+
+        menor = num
+        idx = i
+
+print("Menor valor: {}".format(menor))
+print("Posicao: {}".format(idx))
+
+
+
+    
+
