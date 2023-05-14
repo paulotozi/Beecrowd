48760f6bb91a16dcea2b50cf3de1215cd9001f70
paulotozi <paulotozi1512@gmail.com>
2023-05-14 15:14:23 -0300

    Adicionando 1177.py

---
+++ Python/1177.py
@@ -0,0 +1,19 @@
+N = int(input())
+arr = []
+j = 0
+i = 0
+while(i < 1000):
+    
+    arr.append(j)
+    print('N[{}] = {}'.format(i ,j))
+    
+    if(j < (N - 1)):
+
+        j = j + 1
+    
+    else:
+        
+        j == N - 1
+        j = 0
+
+    i = i + 1
