830cd14a670fcf40ee17862e05ba3b8d1e01ab00
paulotozi <paulotozi1512@gmail.com>
2023-05-17 22:46:10 -0300

    Adicionando 1564.py

---
+++ Python/1564.py
@@ -0,0 +1,20 @@
+while(True):
+
+    try:
+
+        N = int(input())
+        if(N == 0):
+
+            print("vai ter copa!")
+
+        else:
+
+            print("vai ter duas!")
+
+    except ValueError:
+
+        break
+
+    except EOFError:
+
+        break
