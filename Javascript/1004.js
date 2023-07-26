7c4b0d2e7d54e969690905170dd2544b5e39c33f
paulotozi <paulotozi1512@gmail.com>
2023-07-26 12:55:22 -0300

    Adicionando 1004.js

--- Javascript/1004.js
+++ Javascript/1004.js
@@ -4,3 +4,12 @@ var n1 = parseInt(prompt());
 var n2 = parseInt(prompt());
 
 console.log("PROD = %s", n1 * n2);
+
+//solucao do beecrowd
+/*var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var n1 = parseInt(lines.shift());
+var n2 = parseInt(lines.shift());
+
+console.log("PROD = %s", n1 * n2);*/
