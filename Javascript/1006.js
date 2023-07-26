7c7279702a77da1e90ac1bce85b55c286d63355d
paulotozi <paulotozi1512@gmail.com>
2023-07-26 13:52:43 -0300

    Adicionando 1006.js

---
+++ Javascript/1006.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+var n1 = parseFloat(prompt());
+var n2 = parseFloat(prompt());
+var n3 = parseFloat(prompt());
+
+console.log("MEDIA = %s", ((n1 * 0.2) + (n2 * 0.3) + (n3 * 0.5)).toFixed(1));
+
+//solucao do beecrowd
+/*var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var n1 = parseFloat(lines.shift());
+var n2 = parseFloat(lines.shift());
+var n3 = parseFloat(lines.shift());
+
+console.log("MEDIA = %s", ((n1 * 0.2) + (n2 * 0.3) + (n3 * 0.5)).toFixed(1));*/
