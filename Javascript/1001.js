96affc64501b6d4550411ee0451a61c99c83ab5f
paulotozi <paulotozi1512@gmail.com>
2023-07-26 14:14:17 -0300

    Adicionando 1001.js

---
+++ Javascript/1001.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+var n1 = parseInt(prompt());
+var n2 = parseInt(prompt());
+
+console.log("X = %s", n1 + n2);
+
+//solucao do beecrowd
+/*var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var n1 = parseInt(lines.shift());
+var n2 = parseInt(lines.shift());
+
+
+console.log("X = %s", n1 + n2);*/
